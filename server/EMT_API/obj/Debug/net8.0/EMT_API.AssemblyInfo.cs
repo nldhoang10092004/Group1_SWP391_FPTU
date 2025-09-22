9c8a7f8a91c9dcbddc25b54003670f92ae7fe759
Nguyễn Lê Duy Hoàng <114505869+nldhoang10092004@users.noreply.github.com>
2025-09-22 16:16:33 +0700

    change folder name

--- server/EMT_API/obj/Debug/net8.0/EMT_API.AssemblyInfo.cs
+++ server/EMT_API/obj/Debug/net8.0/EMT_API.AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9a3c482b-c937-496c-b973-31db6beae285")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMT_API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfd1778ef0badccf8abfebaf3f4ba6dbf9a36f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMT_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMT_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
