eeaf688946788424d202472060b5b479ddc2ac0e
LOVBE <tranludangtrung@gmail.com>
2025-10-05 11:09:32 +0700

    Add admin

---
+++ server/EMT_API/Controllers/Admin/CourseManagementController.cs
@@ -0,0 +1,18 @@
+﻿using EMT_API.Data;
+using EMT_API.Models;
+using EMT_API.DTOs.Admin;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static EMT_API.Controllers.AuthController;
+using EMT_API.Security;
+using EMT_API.DTOs.Auth;
+
+namespace EMT_API.Controllers.Admin;
+[ApiController]
+[Route("api/admin/courses")]
+[AdminOnly]
+
+public class CourseManagementController : ControllerBase
+{
+    private readonly EMTDbContext _db;
+}
