761a80e694c9b57111da807d4c6ef2f8e17adfe0
jackyy2004 <thejackyynguyen@gmail.com>
2025-10-17 17:34:23 +0700

    Admin và Footer FE

---
+++ front-end/src/components/Admin/index.js
@@ -0,0 +1,9 @@
+// Export all Admin components
+export { default as AdminDashboard } from './Dashboard';
+export { default as Statistics } from './Statistics';
+export { default as UserManagement } from './UserManagement';
+export { default as TeacherManagement } from './TeacherManagement';
+export { default as CourseManagement } from './CourseManagement';
+export { default as ReviewManagement } from './ReviewManagement';
+export { default as ExamManagement } from './ExamManagement';
+export { default as VoucherManagement } from './VoucherManagement';
