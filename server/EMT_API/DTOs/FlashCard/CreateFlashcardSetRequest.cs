5f209d745e866e2f1f8c959c96d81a940dbd24c5
Nguyễn Lê Duy Hoàng <114505869+nldhoang10092004@users.noreply.github.com>
2025-10-19 15:18:46 +0700

    add flashcard API

---
+++ server/EMT_API/DTOs/FlashCard/CreateFlashcardSetRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace EMT_API.DTOs.Flashcard
+{
+    public class CreateFlashcardSetRequest
+    {
+        public int? CourseID { get; set; }  // nếu null => free
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
---
+++ server/EMT_API/DTOs/FlashCard/FlashcardItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EMT_API.DTOs.Flashcard
+{
+    public class FlashcardItemDto
+    {
+        public int ItemID { get; set; }
+        public string FrontText { get; set; } = string.Empty;
+        public string BackText { get; set; } = string.Empty;
+        public string? Example { get; set; }
+    }
+}
