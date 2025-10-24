cf869172f1f6ef2793ff0da1eef24f330c979471
LOVBE <tranludangtrung@gmail.com>
2025-10-24 10:04:48 +0700

    trung xóa update user

---
+++ server/EMT_API/Controllers/Admin/SystemExamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMT_API.Controllers.Admin
+{
+    public class SystemExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
