568722022615612659dbd8cc13f0bb3869d73ecd
LOVBE <tranludangtrung@gmail.com>
2025-10-31 20:00:49 +0700

    score và transaction

---
+++ server/EMT_API/DTOs/Admin/ScoreViewRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace EMT_API.DTOs.Admin
+{
+    public class ScoreViewRequest
+    {
+        public int AttemptId { get; set; }
+        public int QuizId { get; set; }
+        public string QuizTitle { get; set; }
+        public int? CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public double Score { get; set; }
+        public DateTime AttemptDate { get; set; }
+    }
+}
